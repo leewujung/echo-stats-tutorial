90611e51b821900200eec519f7f1013609e92bd0
Wu-Jung Lee <leewujung@gmail.com>
2017-04-17 16:28:02 -0700

    params for new bbechopdf that uses struct as input

---
+++ broadband_code_current/bbechopdf_param.m
@@ -0,0 +1,43 @@
+
+param.c = 1500;
+param.gate_len = 0.5;
+
+param.bp_folder = '/Volumes/wjlee_apl_2/echo_stat_tutorial/echo_stat_figs/make_bpf_pool_20170417/';
+param.bp_file = 'bpf_a0.211m_dtheta0.010pi_fmax1500kHz_df100Hz.mat';
+param.fish_folder = '~/code/echo_stat_tutorial/broadband_code_current/fish_info/';
+param.fish_file = 'fish_scat_response_angle-90to90deg_len19to29cm.mat';
+param.fish_len_folder = '~/code/echo_stat_tutorial/broadband_code_current/fish_info/';
+param.fish_len_file = 'fish_len_dist.mat';
+
+param.tx_opt=1;
+
+param.scatterer.type = 'fish';
+param.scatterer.nbwb = 'wb';
+
+
+% param.gate_length
+% param.save_folder
+% param.save_fname 
+
+% param.c = 1500;  % sounds speed
+
+
+% param.tx_opt
+% param.tx_taper_opt
+% param.bp_folder
+% param.bp_file
+% param.fish_folder
+% param.fish_file
+% param.fish_len_folder
+% param.fish_len_file
+% param.nb_freq
+% param.bpa
+
+% param.scatterer.type = 'rayl'; % point, rayl, prosph, fish
+% param.scatterer.ar
+% param.scatterer.len_bin
+% param.scatterer.len_dist
+% param.scatterer.len_unit
+% param.scatterer.angle_mean
+% param.scatterer.angle_std
+% param.scatterer.angle_unit
