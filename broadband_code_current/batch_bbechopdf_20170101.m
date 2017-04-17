4aed68caa01e9996df303abaa017a66fe55bed34
Wu-Jung Lee <leewujung@gmail.com>
2017-04-17 16:57:09 -0700

    test runnning new bbechopdf code

--- broadband_code_current/batch_bbechopdf_20170101.m
+++ broadband_code_current/batch_bbechopdf_20170101.m
@@ -1,27 +1,34 @@
 
 
+param.c = 1500;
+param.gate_len = 0.5;
+
+param.bp_path = '/Volumes/wjlee_apl_2/echo_stat_tutorial/echo_stat_figs/make_bpf_pool_20170417/';
+param.bp_file = 'bpf_a0.211m_dtheta0.010pi_fmax1500kHz_df100Hz.mat';
+param.fish_path = '~/code/echo_stat_tutorial/broadband_code_current/fish_info/';
+param.fish_file = 'fish_scat_response_angle-90to90deg_len19to29cm.mat';
+param.fish_len_path = '~/code/echo_stat_tutorial/broadband_code_current/fish_info/';
+param.fish_len_file = 'fish_len_dist.mat';
+
+param.scatterer.type = 'fish';
+param.scatterer.nbwb = 'wb';
+
 N = [25,250,2500];
 mix_r = 1;
-ns = 1e3;
-gate_len = 0.5;
-save_opt = 1;
-tx = 1;
+num_sample = 1e3;
 
-save_base_path = '~/Desktop/echo_stat_figs';
-
+save_base_path = '/Volumes/wjlee_apl_2/echo_stat_tutorial/echo_stat_figs/';
 [~,script_name,~] = fileparts(mfilename('fullpath'));
 save_path = fullfile(save_base_path,script_name);
 if ~exist(save_path,'dir')
     mkdir(save_path);
 end
+save_file = 'rayleigh';
 
-sdir = save_path;
+param.save_path = save_path;
+param.save_file = save_file;
 
 for iN=1:length(N)
-    % bbechopdf(N,mix_r,ns,gate_len,tx,sdir,fname,...
-    %           bpa,indiv,[len_bin,len_dist],[angle_mean,angle_std],nb_freq);
-    
-    % Rayleigh
-    bbechopdf(N(iN),mix_r,ns,gate_len,tx,sdir,'rayleigh',[],0);
+    bbechopdf_20170417(N(iN),mix_r,num_sample,param);
 end
 
