3408217fff1ab775ce18a8242b29a642b9a74ba0
nupho27 <35059539+nupho27@users.noreply.github.com>
2018-04-21 23:44:41 +0900

    Add files via upload
    PDF of magnitude of backscattering amplitude for smooth prolate spheroid & rough prolate spheroid

---
+++ Figure_10a.m
@@ -0,0 +1,62 @@
+% This code is for the creation of Fig. 10(a) in the tutorial.
+% Written by BAIK, Kyungmin. 10/18/206
+
+clear
+ae=0.1; % radius of equal volume of sphere in m
+Ep=0.1; % aspect ratio of spheroid
+
+fes=ae/2; % backscattering amplitude for equal volume of sphere.
+
+Fmax=fes*Ep^(-2/3);
+Fmin=fes*Ep^(4/3);
+
+N=10000;
+%Fran=linspace(1.001*Fmin,0.999*Fmax,N);
+Fran=linspace(1.0000001*Fmin,0.9999999*Fmax,N);
+
+%PDF=((Ep^(2/3))*fes)./(2*(Fran.^(3/2))*sqrt(1-Ep^2).*sqrt(fes-(Ep^(2/3))*Fran)); % 3D space
+PDF=((Ep^(2/3))*fes)./(pi*Fran.*sqrt(fes-(Ep^(2/3))*Fran).*sqrt(Fran-(Ep^(4/3))*fes)); % 2D space
+
+% Sfac=trapz(Fran,PDF);
+% PDF=PDF/Sfac;
+
+Nfac=sqrt(trapz(Fran,(Fran.^2).*PDF)); % normalization factor for fs
+
+loglog(Fran/Nfac,PDF/trapz(Fran/Nfac,PDF),'r','LineWidth',2)
+
+
+hold on
+
+Ep=0.2; % aspect ratio of spheroid
+
+Fmax=fes*Ep^(-2/3);
+Fmin=fes*Ep^(4/3);
+
+Fran=linspace(1.0000001*Fmin,0.9999999*Fmax,N);
+
+%PDF=((Ep^(2/3))*fes)./(2*(Fran.^(3/2))*sqrt(1-Ep^2).*sqrt(fes-(Ep^(2/3))*Fran)); % 3D space
+PDF=((Ep^(2/3))*fes)./(pi*Fran.*sqrt(fes-(Ep^(2/3))*Fran).*sqrt(Fran-(Ep^(4/3))*fes)); % 2D space
+
+Nfac=sqrt(trapz(Fran,(Fran.^2).*PDF)); % normalization factor for fs
+
+loglog(Fran/Nfac,PDF/trapz(Fran/Nfac,PDF),'g','LineWidth',2)
+
+Ep=0.5; % aspect ratio of spheroid
+
+Fmax=fes*Ep^(-2/3);
+Fmin=fes*Ep^(4/3);
+
+Fran=linspace(1.0000001*Fmin,0.9999999*Fmax,N);
+
+%PDF=((Ep^(2/3))*fes)./(2*(Fran.^(3/2))*sqrt(1-Ep^2).*sqrt(fes-(Ep^(2/3))*Fran)); % 3D space
+PDF=((Ep^(2/3))*fes)./(pi*Fran.*sqrt(fes-(Ep^(2/3))*Fran).*sqrt(Fran-(Ep^(4/3))*fes)); % 2D space
+
+Nfac=sqrt(trapz(Fran,(Fran.^2).*PDF)); % normalization factor for fs
+
+loglog(Fran/Nfac,PDF/trapz(Fran/Nfac,PDF),'b','LineWidth',2)
+
+%legend('10:1','5:1','2:1')
+%grid on
+
+xlabel('$|{\it f_{ss}}|/<|{\it f_{ss}}|^2>^{1/2}$','interpreter','latex','FontSize',14)
+ylabel('${\it p_{ss}}(|{\it f_{ss}}|)$','interpreter','latex','FontSize',14)
---
+++ Figure_10b.m
@@ -0,0 +1,96 @@
+% This code is for the creation of Fig. 10(b) in the tutorial.
+% Written by BAIK, Kyungmin. 10/18/206
+
+ae=0.1; % radius of equal volume of sphere in m
+Ep=0.1; % aspect ratio of spheroid
+
+fes=ae/2; % backscattering amplitude for equal volume of sphere.
+
+Fmax=fes*Ep^(-2/3);
+Fmin=fes*Ep^(4/3);
+
+N=1000;
+Fran=linspace(1.01*Fmin,0.99*Fmax,N);
+
+%PDF=((Ep^(2/3))*fes)./(2*(Fran.^(3/2))*sqrt(1-Ep^2).*sqrt(fes-(Ep^(2/3))*Fran)); % PDF for spheroid 3D
+PDF=((Ep^(2/3))*fes)./(pi*Fran.*sqrt(fes-(Ep^(2/3))*Fran).*sqrt(Fran-(Ep^(4/3))*fes)); % 2D space
+
+%PDF=PDF/trapz(Fran,PDF); % normalization to confirm integration of PDF over Fran is unity.
+
+NT=1e4;
+
+Ftotal=logspace(-5,2,NT);
+PDFtotal=zeros(size(Ftotal));
+
+for m=1:NT
+    Rran=fliplr(Ftotal(m)./Fran); % Range of Rayleigh distribution
+    PDFtotal(m)=trapz(Rran,exp(-0.5*(Rran.^2)).*fliplr(PDF));
+end
+
+% Sfac=trapz(Ftotal,PDFtotal);
+% PDFtotal=PDFtotal/Sfac;
+
+Nfac=sqrt(trapz(Ftotal,(Ftotal.^2).*PDFtotal)); % normalization factor for fs
+
+loglog(Ftotal/Nfac,PDFtotal/trapz(Ftotal/Nfac,PDFtotal),'r','LineWidth',2)
+axis([1e-3 1e2 1e-6 1e3])
+
+hold on
+
+Ep=0.2; % aspect ratio of spheroid
+
+Fmax=fes*Ep^(-2/3);
+Fmin=fes*Ep^(4/3);
+
+Fran=linspace(1.01*Fmin,0.99*Fmax,N);
+
+%PDF=((Ep^(2/3))*fes)./(2*(Fran.^(3/2))*sqrt(1-Ep^2).*sqrt(fes-(Ep^(2/3))*Fran)); % PDF for spheroid 3D
+PDF=((Ep^(2/3))*fes)./(pi*Fran.*sqrt(fes-(Ep^(2/3))*Fran).*sqrt(Fran-(Ep^(4/3))*fes)); % 2D space
+
+%PDF=PDF/trapz(Fran,PDF); % normalization to confirm integration of PDF over Fran is unity.
+
+Ftotal=logspace(-5,2,NT);
+PDFtotal=zeros(size(Ftotal));
+
+for m=1:NT
+    Rran=fliplr(Ftotal(m)./Fran); % Range of Rayleigh distribution
+    PDFtotal(m)=trapz(Rran,exp(-0.5*(Rran.^2)).*fliplr(PDF));
+end
+
+Nfac=sqrt(trapz(Ftotal,(Ftotal.^2).*PDFtotal)); % normalization factor for fs
+loglog(Ftotal/Nfac,PDFtotal/trapz(Ftotal/Nfac,PDFtotal),'g','LineWidth',2)
+
+Ep=0.5; % aspect ratio of spheroid
+
+Fmax=fes*Ep^(-2/3);
+Fmin=fes*Ep^(4/3);
+
+Fran=linspace(1.01*Fmin,0.99*Fmax,N);
+
+%PDF=((Ep^(2/3))*fes)./(2*(Fran.^(3/2))*sqrt(1-Ep^2).*sqrt(fes-(Ep^(2/3))*Fran)); % PDF for spheroid 3D
+PDF=((Ep^(2/3))*fes)./(pi*Fran.*sqrt(fes-(Ep^(2/3))*Fran).*sqrt(Fran-(Ep^(4/3))*fes)); % 2D space
+
+%PDF=PDF/trapz(Fran,PDF); % normalization to confirm integration of PDF over Fran is unity.
+
+Ftotal=logspace(-5,2,NT);
+PDFtotal=zeros(size(Ftotal));
+
+for m=1:NT
+    Rran=fliplr(Ftotal(m)./Fran); % Range of Rayleigh distribution
+    PDFtotal(m)=trapz(Rran,exp(-0.5*(Rran.^2)).*fliplr(PDF));
+end
+
+Nfac=sqrt(trapz(Ftotal,(Ftotal.^2).*PDFtotal)); % normalization factor for fs
+loglog(Ftotal/Nfac,PDFtotal/trapz(Ftotal/Nfac,PDFtotal),'b','LineWidth',2)
+
+Ftotal=logspace(-5,2,NT);
+PDFtotal=Ftotal.*exp(-(Ftotal.^2)/2);
+
+Nfac=sqrt(trapz(Ftotal,(Ftotal.^2).*PDFtotal)); % normalization factor for fs
+loglog(Ftotal/Nfac,PDFtotal/trapz(Ftotal/Nfac,PDFtotal),'k','LineWidth',2)
+
+legend('10:1','5:1','2:1','1:1 (sphere)')
+axis([1e-3 1e2 1e-6 1e3])
+%grid on
+xlabel('$|{\it f_{ss.}}|/<|{\it f_{ss.}}|^{2}>^{1/2}$','interpreter','latex','FontSize',14)
+ylabel('${\it p_{ss.}}(|{\it f_{ss.}}|)$','interpreter','latex','FontSize',14)
