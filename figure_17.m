8b4b9a8180d2d643f4ac3d03e0b0b4712e443d6d
leewujung <leewujung@gmail.com>
2018-04-23 22:21:08 -0700

    rename and update figure_17; still need testing

--- figure_17.m
+++ figure_17.m
@@ -1,3 +1,11 @@
+% Code to generate Figure 17 of the echo statistics tutorial
+% This figure shows distributions of magnitude of echo in backscatter
+% direction from N randomly rough, randomly oriented prolate spheroids
+% randomly and uniformly distributed in a thin hemispherical shell
+%
+% Author: Wu-Jung Lee | leewujung@gmail.com | APL-UW
+
+
 % 2017 01 01  Prolate spheroid scatterer with and without beampattern, PDF & PFA
 % 2017 04 17  1) Change prolate spheroid orientaiton to 3D instead of 2D
 %                2D-rotating within plane containing MRA
@@ -6,12 +14,8 @@
 %             3) Update figure legend, axis labels, and curve style
 
 clear
-
-addpath '~/Dropbox/0_CODE/MATLAB/saveSameSize/'
-addpath '~/Dropbox/0_CODE/prolatespheroid/'
-
-% base_path = '~/Desktop/echo_stat_figs';
-base_path = '/Volumes/wjlee_apl_2 1/echo_stat_tutorial/echo_stat_figs/';
+addpath './util_fcn'
+base_path = './figs';
 
 % Make save path
 str = strsplit(mfilename('fullpath'),'/');
@@ -22,9 +26,9 @@ if ~exist(save_path,'dir')
 end
 
 % Set param
-X = load('fig_12_pb_ka_ka_num.mat');
+X = load('./figs/figure_12/figure_12_ka_num.mat');
 ka = X.ka_3deg;
-% ka = 2*pi;
+
 sph_rot_opt = '3D';
 
 pingnum_str = '1e7';
@@ -32,75 +36,66 @@ pingnum = eval(pingnum_str);
 
 npt = 200;  % number of points for pe kde estimation
 
-if ~exist(save_path,'dir')
-    mkdir(save_path);
-end
-
-% N_all = [1,10,100,1000];
 v_rayl = 1/sqrt(2);
+N_all = [1,2,5,10,100,1000];
 
-% leg_str = cell(1,length(N_all)+1);
-% leg_str{1} = 'Rayleigh';
-% for iN=2:length(N_all)+1
-%     leg_str{iN} = sprintf('N=%d',N_all(iN-1));
-% end
+% Set operation
+mc_opt = 1;  % 0 - do not re-generate realizations
+             % 1 - re-generate all realizations
 
-
-if 0
-
-for iN=1:length(N_all)
-    Ns = N_all(iN);
-    
-    param.N = Ns;
-    param.ka = ka;
-    
-    parfor iP = 1:pingnum
-        phase = unifrnd(0,2*pi,Ns,1);
+% Monte Carlo simulation
+if mc_opt
+    for iN=1:length(N_all)
+        Ns = N_all(iN);
         
-        cc = 1;
-        e_ac = 1/10;
-        b1 = cc*e_ac; % length of semi-minor axis
-        if strcmp(sph_rot_opt,'2D')    % Constrain spheroid rotation in MRA plane
-            theta_sph = unifrnd(0,2*pi,Ns,1);  % before 2017/04, 
-        elseif strcmp(sph_rot_opt,'3D')  % theta_sph follow sin(theta_sph) in 3D spherical coord
+        param.N = Ns;
+        param.ka = ka;
+        
+        parfor iP = 1:pingnum
+            phase = unifrnd(0,2*pi,Ns,1);
+            
+            cc = 1;
+            e_ac = 1/10;
+            b1 = cc*e_ac; % length of semi-minor axis
+            if strcmp(sph_rot_opt,'2D')    % Constrain spheroid rotation in MRA plane
+                theta_sph = unifrnd(0,2*pi,Ns,1);  % before 2017/04,
+            elseif strcmp(sph_rot_opt,'3D')  % theta_sph follow sin(theta_sph) in 3D spherical coord
+                u = unifrnd(0,1,Ns,1);
+                theta_sph = pi/2-acos(u);  % theta_sph calculated from normal incidence
+            end
+            fss = cc/2.*sin(atan(b1./(cc.*tan(theta_sph)))).^2./cos(theta_sph).^2;
+            roughness = raylrnd(ones(Ns,1)*1/sqrt(2));
+            amp = fss.*roughness;
+            
+            s = amp.*exp(1i*phase);
+            
+            % position in the beam
             u = unifrnd(0,1,Ns,1);
-            theta_sph = pi/2-acos(u);  % theta_sph calculated from normal incidence
-        end
-        fss = cc/2.*sin(atan(b1./(cc.*tan(theta_sph)))).^2./cos(theta_sph).^2;
-        roughness = raylrnd(ones(Ns,1)*1/sqrt(2));
-        amp = fss.*roughness;
+            theta = acos(u);
+            
+            % beampattern modulation
+            b_bp1 = (2*besselj(1,ka*sin(theta))./(ka*sin(theta))).^2;
+            b_bp0 = 1;
+            
+            % E=SB
+            e_bp0 = s.*b_bp0;
+            e_bp1 = s.*b_bp1;
+            
+            env_bp0(iP) = abs(sum(e_bp0));
+            env_bp1(iP) = abs(sum(e_bp1));
+            
+        end % pingnum loop
         
-        s = amp.*exp(1i*phase);
+        env = env_bp0;
+        file_save = sprintf('pnum_%s_ka%2.4f_N%04d_%s_bp0.mat',...
+            pingnum_str,ka,Ns,sph_rot_opt);
+        save([save_path,'/',file_save],'env','param');
         
-        % position in the beam
-        u = unifrnd(0,1,Ns,1);
-        theta = acos(u);
-        
-        % beampattern modulation
-        b_bp1 = (2*besselj(1,ka*sin(theta))./(ka*sin(theta))).^2;
-        b_bp0 = 1;
-        
-        % E=SB
-        e_bp0 = s.*b_bp0;
-        e_bp1 = s.*b_bp1;
-        
-        env_bp0(iP) = abs(sum(e_bp0));
-        env_bp1(iP) = abs(sum(e_bp1));
-        
-    end % pingnum loop
-    
-    env = env_bp0;
-    file_save = sprintf('pnum_%s_ka%2.4f_N%04d_%s_bp0.mat',...
-        pingnum_str,ka,Ns,sph_rot_opt);
-    save([save_path,'/',file_save],'env','param');
-    
-    env = env_bp1;
-    file_save = sprintf('pnum_%s_ka%2.4f_N%04d_%s_bp1.mat',...
-        pingnum_str,ka,Ns,sph_rot_opt);
-    save([save_path,'/',file_save],'env','param');
-    
-end
-
+        env = env_bp1;
+        file_save = sprintf('pnum_%s_ka%2.4f_N%04d_%s_bp1.mat',...
+            pingnum_str,ka,Ns,sph_rot_opt);
+        save([save_path,'/',file_save],'env','param');
+    end
 end
 
 
@@ -141,7 +136,6 @@ xlabel('$\tilde{e}/<\tilde{e}^2>^{1/2}$','Interpreter','LaTex','fontsize',24);
 ylabel('$p_e(\tilde{e}/<\tilde{e}^2>^{1/2})$','Interpreter','LaTex','fontsize',24);
 xlim([1e-3 1e2]);
 ylim([1e-6 1e3]);
-% title('PDF')
 
 save_fname = sprintf('%s_ka%2.4f_smpl%s_%s_pdf_bp0',...
     str,ka,pingnum_str,sph_rot_opt);
@@ -187,7 +181,6 @@ xlabel('$\tilde{e}/<\tilde{e}^2>^{1/2}$','Interpreter','LaTex','fontsize',24);
 ylabel('$p_e(\tilde{e}/<\tilde{e}^2>^{1/2})$','Interpreter','LaTex','fontsize',24);
 xlim([1e-3 1e3]);
 ylim([1e-7 1e3]);
-% title('PDF')
 
 save_fname = sprintf('%s_ka%2.4f_smpl%s_%s_pdf_bp1',...
     str,ka,pingnum_str,sph_rot_opt);
@@ -237,7 +230,6 @@ xlabel('$\tilde{e}/<\tilde{e}^2>^{1/2}$','Interpreter','LaTex','fontsize',24);
 ylabel('$PFA(\tilde{e}/<\tilde{e}^2>^{1/2})$','Interpreter','LaTex','fontsize',24);
 xlim([1e-3 1e3]);
 ylim([1e-6 1e1]);
-% title('PFA')
 
 save_fname = sprintf('%s_ka%2.4f_smpl%s_%s_pfa_bp1',...
     str,ka,pingnum_str,sph_rot_opt);
