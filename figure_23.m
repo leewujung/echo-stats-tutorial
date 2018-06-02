fe6f52deb3ba15cc7319cb2cf9c3f686f3331050
leewujung <leewujung@gmail.com>
2018-06-02 17:14:31 -0400

    update various paths and check for bpf lookup table in figure_23.m

--- figure_23.m
+++ figure_23.m
@@ -1,6 +1,15 @@
 % Code to generate Figure 23 of the echo statistics tutorial.
 % This figure shows the comparisons between PDFs of echo magnitudes
-% associated with long narrowband and short-pulsed broadband signals
+% associated with long narrowband and short-pulsed broadband signals.
+%
+% Package 'broadband-echo-stats' is used to generate the ensemble of
+% samples for both narrowband and broadband echo pdfs for this comparison
+% figure. This package is located at: https://github.com/leewujung/broadband-echo-stats
+% If you have cloned this code from GitHub this package is included as a
+% submodule. To make sure you have the code, run 
+%     git submodule init
+% and then
+%     git submodule update
 %
 % Author: Wu-Jung Lee | leewujung@gmail.com | APL-UW
 
@@ -10,8 +19,10 @@
 
 clear
 addpath './util_fcn'
-addpath './broadband_code_current'
+addpath './broadband-echo-stats'
 base_path = './figs';
+bpf_path = './figs/make_bpf_pool';
+fish_info_path = './broadband-echo-stats/fish_info';
 
 % Make save path
 str = strsplit(mfilename('fullpath'),'/');
@@ -21,11 +32,18 @@ if ~exist(save_path,'dir')
     mkdir(save_path);
 end
 
+% Test if already have broadband beampattern response lookup table
+% if not, run code to generate one
+if ~exist(bpf_path,'dir')
+    disp('Do not have broadband beampattern response lookup table.');
+    disp('Generating one...');
+    make_bpf_pool;
+end
 
 % Set overall params
 N = [25,250,2500];
 mix_r = ones(length(N),1);
-num_sample_str = '5e5';
+num_sample_str = '1e2';
 num_sample = eval(num_sample_str);
 save_file_pre = 'rayleigh';
 
@@ -33,11 +51,11 @@ save_file_pre = 'rayleigh';
 param.scatterer.type = 'rayl';
 param.c = 1500;
 param.gate_len = 0.5;
-param.bp_path = '/Volumes/wjlee_apl_2 1/echo_stat_tutorial/echo_stat_figs/make_bpf_pool_20170417/';
+param.bp_path = bpf_path;
 param.bp_file = 'bpf_a0.211m_dtheta0.001pi_fmax1500kHz_df100Hz.mat';
-param.fish_path = '~/code/echo_stat_tutorial/broadband_code_current/fish_info/';
+param.fish_path = fish_info_path;
 param.fish_file = 'fish_scat_response_angle-90to90deg_len19to29cm.mat';
-param.fish_len_path = '~/code/echo_stat_tutorial/broadband_code_current/fish_info/';
+param.fish_len_path = fish_info_path;
 param.fish_len_file = 'fish_len_dist.mat';
 
 
@@ -49,12 +67,14 @@ mc_opt = 1;  % 0 - do not re-generate realizations
 if mc_opt
     % Run bbechopdf code
     for iN=1:length(N)
+        disp('Generating broadband samples');
         param.nbwb = 'wb';
         [s, param] = bbechopdf_20170417(N(iN),mix_r,num_sample,param);
         sfname = sprintf('%s_N_%04d_r_%02d_pnum%s_glen%2.2f_wb.mat',...
                 save_file_pre,N(iN),mix_r(iN),num_sample_str,param.gate_len);
         save([save_path,'/',sfname],'param','s');
 
+        disp('Generating narrowband samples');
         param.nbwb = 'nb';
         [s, param] = bbechopdf_20170417(N(iN),mix_r,num_sample,param);
         sfname = sprintf('%s_N_%04d_r_%02d_pnum%s_glen%2.2f_nb.mat',...
@@ -105,9 +125,9 @@ for iN=1:length(N)
 
     % Save figure
     save_fname = sprintf('%s_N_%04d_r_%02d_pnum%s_glen%2.2f',...
-        script_name,N(iN),mix_r(iN),num_sample_str,param.gate_len);
+        str,N(iN),mix_r(iN),num_sample_str,param.gate_len);
     saveas(fig,[fullfile(save_path,save_fname),'.fig'],'fig');
-    saveSameSize_100(fig,'file',[fullfile(save_path,save_fname),'.png'],...
+    saveSameSize(fig,'file',[fullfile(save_path,save_fname),'.png'],...
         'format','png');
 
 end
